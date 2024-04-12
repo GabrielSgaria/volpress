09d8d5125009bd27f2ca99323d3e807ada8c98c4
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-04-12 12:00:11 -0300

    feat: footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <div className="h-[2000px] bg-white text-zinc-950">
       <section className="bg-cover bg-top bg-no-repeat from-cyan-500 to-blue-500 bg-[url('../../public/image/frota/frotabg.jpg')] shadow-2xl"  >
-      
+
         <NavBar />
         <div className='flex flex-row w-full h-[650px] sm:h-[700px] items-center '>
           <div className='container mx-auto px-8 flex z-30 -mt-72' >
@@ -45,7 +45,7 @@ export default function Home() {
       <section className='w-full py-24 bg-zinc-50'>
         <div className='container mx-auto flex items-center gap-20 flex-col px-4 sm:px-0'>
           <div className='flex flex-col container mx-auto justify-center items-center z-40 relative gap-5'>
-            <h1 className='font-black text-4xl sm:text-5xl uppercase text-zinc-700'>Nossa Frota</h1>
+            <h1 className='font-black text-4xl uppercase text-zinc-700'>Nossa Frota</h1>
             <p className='max-w-[700px] text-center text-zinc-900'>Nossa frota é moderna e padronizada para atender às exigências dos clientes. Utilizamos carretas LS e Vanderleia,
               com no mínimo 14 metros de comprimento e equipadas com bobineiras para maior flexibilidadenpm run dev no transporte.
               Isso nos permite oferecer maior capacidade de carga, resultando em melhor custo-benefício para os clientes.</p>
@@ -55,13 +55,16 @@ export default function Home() {
       </section>
       <section className='bg-zinc-200 py-16'>
         <div className='flex flex-col container mx-auto justify-center items-center z-40 relative gap-9'>
-          <h1 className='font-black text-4xl sm:text-5xl uppercase text-zinc-700'>NOSSOS CLIENTES</h1>
+          <h1 className='font-black text-4xl uppercase text-zinc-700'>NOSSOS CLIENTES</h1>
           <p className='max-w-[650px] text-center text-zinc-600'>Nossos clientes são de extrema importância para nós, e valorizamos cada um deles de forma igualitária. Aqui estão alguns exemplos destacados</p>
         </div>
         <ClientsCarrousel />
       </section>
-      <section className='bg-zinc-50'>
-          
+      <section className='bg-zinc-50 py-16'>
+        <div className='flex flex-col container mx-auto justify-center items-center z-40 relative gap-9'>
+          <h1 className='font-black text-4xl uppercase text-zinc-700'>SERVIÇOS VOLPRESS</h1>
+          <p className='max-w-[650px] text-center text-zinc-600'>Seja qual for o serviço contratado, não abrimos mãos da pontualidade e segurança, pontualidade é a nossa meta diária, e segurança a nossa obsessão. </p>
+        </div>
       </section>
 
       <Footer />
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,8 +3,9 @@ import {
   DesktopIcon,
   EnvelopeClosedIcon,
 } from "@radix-ui/react-icons";
-import { FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaPhone, FaPhoneSlash, FaPhoneSquareAlt, FaWhatsapp } from "react-icons/fa";
 import Link from "next/link";
+import { FaLocationDot, FaPhoneFlip } from "react-icons/fa6";
 
 export function Footer() {
   function sendMensage(numeroTelefone: number, mensagemPadrao: string) {
@@ -18,7 +19,7 @@ export function Footer() {
   }
 
   return (
-    <footer id="contacts" className="pb-8">
+    <footer id="contacts" className="pb-8 border-t border-zinc-300/80">
       <div className="container mx-auto mt-16 flex flex-col gap-16 sm:flex-row sm:justify-around sm:gap-0">
         <div className="flex flex-col items-center sm:items-start ">
           <h1 className="text-2xl font-semibold bebasNeue text-zinc-800">
@@ -33,7 +34,7 @@ export function Footer() {
             <a
               onClick={() =>
                 sendMensage(
-                  5541997169203,
+                  5541995020104,
                   "Olá! Vim através do site da M2 Agência de Marketing."
                 )
               }
@@ -41,17 +42,48 @@ export function Footer() {
             >
               <FaWhatsapp className="size-6" />
               <p className="h-6 text-base items-center justify-center font-semibold transition-all duration-500 hover:border-b-[1px] hover:border-zinc-800">
-                (41) 99716-9203
+                (41) 99502-0104
+              </p>
+            </a>
+            <a
+              href="tel:+554133490338"
+              className="flex cursor-pointer items-center justify-center gap-4 text-zinc-800"
+            >
+              <FaPhoneFlip className="size-5" />
+              <p className="h-6 text-base items-center justify-center font-semibold transition-all duration-500 hover:border-b-[1px] hover:border-zinc-800">
+                (41) 3349-0338
               </p>
             </a>
             <Link
               target="blank"
-              href="mailto:m2agdigital@gmail.com"
+              href="mailto:transportes@volpress.com.br"
               className="flex cursor-pointer items-center gap-4 text-zinc-800"
             >
               <EnvelopeClosedIcon className="size-6" />
               <p className="h-6 font-semibold text-base transition-all duration-500 hover:border-b-[1px] hover:border-zinc-800">
-                m2agdigital@gmail.com
+                transportes@volpress.com.br
+              </p>
+            </Link>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center sm:items-start">
+          <h1 className="text-center text-2xl font-semibold text-zinc-800 sm:text-start ">
+            Venha <br /> nos conhecer
+          </h1>
+          <p className="mt-8 max-w-[300px] text-center sm:text-start text-xl sm:text-base text-zinc-600">
+            Volpress Transportes - Matriz
+          </p>
+          <div className="mt-8 flex flex-col items-center gap-4 sm:items-start justify-center">
+
+            <Link
+              target="blank"
+              href="https://maps.app.goo.gl/WEy6ZKgSTVQibxew9"
+              className="flex cursor-pointer flex-row items-center gap-4 text-zinc-800"
+            >
+              <FaLocationDot className="size-6" />
+              <p className="h-full font-semibold text-base transition-all duration-500 hover:border-b-[1px] hover:border-zinc-800">
+                Rod BR 116, Km 110, n. 22.730 Sala 2 <br />Tatuquara - 81690-500 - Curitiba PR
               </p>
             </Link>
           </div>
@@ -66,34 +98,7 @@ export function Footer() {
             explorar oportunidades imediatamente!
           </p>
         </div>
-        <div className="flex flex-col items-center sm:items-start">
-          <h1 className="text-center text-2xl font-semibold text-zinc-800 sm:text-start ">
-            Nos encontre nas <br />
-            redes sociais
-          </h1>
-          <div className="mt-8 flex flex-col items-center gap-4 sm:items-start">
-            <Link
-              target="blank"
-              href="https://www.facebook.com/profile.php?id=100093656801783"
-              className="flex cursor-pointer flex-row items-center gap-4 text-zinc-800"
-            >
-              <FaFacebook className="size-6" />
-              <p className="h-6 text-base font-semibold transition-all duration-500 hover:border-b-[1px] hover:border-zinc-800">
-                M2 Agência de Marketing
-              </p>
-            </Link>
-            <Link
-              target="blank"
-              href="https://www.instagram.com/m2agenciamkt/"
-              className="flex cursor-pointer flex-row items-center gap-4 text-zinc-800"
-            >
-              <FaInstagram className="size-6" />
-              <p className="h-6 font-semibold text-base transition-all duration-500 hover:border-b-[1px] hover:border-zinc-800">
-                @m2agenciamkt
-              </p>
-            </Link>
-          </div>
-        </div>
+
       </div>
       <div className="container mx-auto mt-16 flex h-8 cursor-pointer items-center justify-center space-x-2 text-zinc-500 transition-all  hover:font-semibold hover:text-zinc-950">
         <h1 className="max-w-[300px] text-sm sm:text-start ">
