2afddfeffbbeee75498732103c0fcca36496dc1d
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-04-15 16:58:40 -0300

    fix: location

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import { SobreNos } from '@/components/sobreNos'
 import { Footer } from '@/components/footer'
 import { Truck, TruckTrailer } from '@phosphor-icons/react';
 import { CardServices } from '@/components/card-services';
-import { Location } from '@/components/location';
+import GoogleMaps from '@/components/google-maps';
 
 
 export default function Home() {
@@ -74,7 +74,17 @@ export default function Home() {
         </div>
         <ClientsCarrousel />
       </section>
-      <Location />
+      <section className='w-full pt-16 bg-zinc-50' id="location">
+        <div className='flex flex-col container mx-auto justify-center items-center z-40 relative gap-9'>
+          <h1 className='font-black text-3xl sm:text-4xl uppercase text-zinc-700'>ONDE ESTAMOS LOCALIZADOS</h1>
+          <p className='max-w-[650px] text-center text-zinc-600 px-3'>
+            Nossa matriz esta localizada na Rod BR 116, Km 110, n. 22.730 Sala 2 - Tatuquara - 81690-500 -
+            Curitiba PR. Esse ponto estratégico nos permite atender com maior agilidade os estados de Minas Gerais,
+            Espírito Santo, Rio de Janeiro, São Paulo, Paraná, Santa Catarina e Rio Grande do Sul, entre outros.
+          </p>
+        </div>
+        <GoogleMaps />
+      </section>
       <Footer />
     </div >
   )
