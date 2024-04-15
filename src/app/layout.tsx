4a6972c36461122def927bd6b18ba16dc1e6ed78
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-04-15 16:45:34 -0300

    feat: metadata volpress

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,47 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Volpress - Transportadora',
   description: 'Transportadora volpress',
+  metadataBase: new URL("https://volpress.vercel.app"),
+  icons: '/favicon.png',
+  openGraph: {
+    title: "Volpress - Transportadora",
+    description: "Volpress - Transportadora",
+    url: "Volpress - Transportadora",
+    siteName: "Volpress - Transportadora",
+    images: [
+      {
+        url: "https://opengraph.b-cdn.net/production/documents/55f2a900-dd3a-4794-bbcb-ea57de67c326.jpg?token=3ofuTBWxl8CiUfsbV6OHNNm873VfpdW9r1gWc-P-Rk8&height=1009&width=1200&expires=33249210163",
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "https://opengraph.b-cdn.net/production/documents/55f2a900-dd3a-4794-bbcb-ea57de67c326.jpg?token=3ofuTBWxl8CiUfsbV6OHNNm873VfpdW9r1gWc-P-Rk8&height=1009&width=1200&expires=33249210163",
+        width: 1800,
+        height: 1600,
+      },
+    ],
+    locale: "pt_BR",
+    type: "website"
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+    },
+  },
+  keywords: ["transportadora de carga", "logística", "entrega rápida", "transporte de mercadorias",
+    "transporte de carga", "transporte terrestre", "transportadora de encomendas", "serviços logísticos",
+    "transporte rodoviário", "frete", "logística integrada", "transportadora expressa", "distribuição de cargas",
+    "serviços de transporte", "transporte de produtos", "logística eficiente", "logística de distribuição",
+    "soluções logísticas", "logística de transporte", "transporte de carga fracionada", "entrega de encomendas",
+    "logística de armazenagem", "transporte de carga pesada", "entrega programada", "coleta e entrega de mercadorias",
+    "transportadora", "volpress", "volpress transportes"
+  ]
 }
-
 export default function RootLayout({
   children,
 }: Readonly<{
